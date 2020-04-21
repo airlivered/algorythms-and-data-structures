c62af92d8e7bf0139173478f88d4fdf1c17cd535
Kiril Bonduryansky <55139428+airlivered@users.noreply.github.com>
2020-04-21 20:08:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+
+
+
+int main()
+{
+int table[500];
+
+char t[100],p[25];
+int n,i,k,j,m,flag=0;
+printf("Enter the text : ");
+gets(t);
+n=strlen(t);
+printf("Enter the pattren : ");
+gets(p);
+m=strlen(p);
+for(i=0; i<500; i++)
+table[i]=m;
+for(j=0;j<m-2;j++)
+table[p[j]]=m-1-j;
+i=m-1;
+while(i<=n-1)
+{
+    k=0;
+    while(k<=m-1 && p[m-1-k]==t[i-k])
+    k++;
+    if(k==m)
+    {
+        printf("The position of the pattern is %d\n",i-m+2);
+        flag=1;
+        break;
+    }
+    else
+        i=i+table[t[i]];
+ }
+if(!flag){
+    printf("Pattern is not found in the given text ");
+}
+
+return 0;
+}
